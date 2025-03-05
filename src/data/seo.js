44713be706acc4c7b5c1ab470ca33e420448b432
Jiewen Huang <jiewenhu@buffalo.edu>
2025-03-05 16:57:18 -0500

    fixing the seo.js page with my information

--- src/data/seo.js
+++ src/data/seo.js
@@ -1,38 +1,38 @@
 const SEO = [
-	{
-		page: "home",
-		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
+    {
+        page: "home",
+        description:
+            "I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+        keywords: ["Jiewen", "Jiewen Huang", "Jiewen Huang Developer"],
+    },
 
-	{
-		page: "about",
-		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
+    {
+        page: "about",
+        description:
+            "I am passionate about building, learning, and solving problems and sometimes with one too many questions along the way. I thrive on curiosity and love diving into new technologies, always eager to expand my knowledge. When I’m not coding, you can find me at the gym, playing games, or working on something crafty (new recent hobby would be scrapbooking).",
+        keywords: ["Jiewen", "Jiewen Huang", "Jiewen Huang Developer"],
+    },
 
-	{
-		page: "articles",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
+    {
+        page: "articles",
+        description:
+            "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+        keywords: ["Jiewen", "Jiewen Huang", "Jiewen Huang Developer"],
+    },
 
-	{
-		page: "projects",
-		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
+    {
+        page: "projects",
+        description:
+            "I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+        keywords: ["Jiewen", "Jiewen Huang", "Jiewen Huang Developer"],
+    },
 
-	{
-		page: "contact",
-		description:
-			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
+    {
+        page: "contact",
+        description:
+            "If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+        keywords: ["Jiewen", "Jiewen Huang", "Jiewen Huang Developer"],
+    },
 ];
 
 export default SEO;
